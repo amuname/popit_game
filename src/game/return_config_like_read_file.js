cb35eef4d025c180b9c30f77a49d371204575ba0
amuname <74045707+amuname@users.noreply.github.com>
2021-06-06 08:46:19 -0700

    Create return_config_like_read_file.js

---
+++ src/game/return_config_like_read_file.js
@@ -0,0 +1,145 @@
+const level_data = 
+		{
+			// __proto__:game_fields,
+			"data":{
+				"user_name":"1337",
+			},
+			"game_field":{
+				"type":"1",
+			}
+		}
+
+const game_fields = {
+	"1":{
+		"background":"fields/1.png",
+		"html":{
+			"header":{
+				"menu":"go back",
+				"level_info":"Level 1",
+				"user_score":""
+			},
+			"game_layout":[
+			    [
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": "2"
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": "8"
+			        },
+			    ],
+			    [
+			        {
+			            "clicked": false,
+			            "secret": "1"
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": "4"
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": "7"
+			        },
+			    ],
+			    [
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": "3"
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": "6"
+			        },
+			    ],
+			    [
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			        {
+			            "clicked": false,
+			            "secret": ""
+			        },
+			    ]
+			]
+		}
+	},
+}
